58ed7d284b309c330d5616d774026154ca51254f
alewtina <alewtina_f@mail.ru>
2018-08-22 21:23:52 +0000

    added company to user

--- app/models/spree/seller.rb
+++ app/models/spree/seller.rb
@@ -1,3 +1,11 @@
 class Spree::Seller <  Spree::Base
-	belongs_to :user
+
+ 	belongs_to :user, class_name: Spree.user_class.to_s, optional: true
+
+	def self.default(user = nil)
+	    if user && user.seller
+	    else
+	     new(company_name: '')
+	    end	
+	end
 end
