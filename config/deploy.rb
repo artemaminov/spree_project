24db9209c9d726b4f4479fb532b458de286c6156
Yegor Danchenko <yegordanchenko@gmail.com>
2019-07-11 12:57:44 +0300

    Remove unused

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
-set :branch, ENV['BRANCH'] ? ENV['BRANCH'] : 'develop'
+
 #set :application, "my_app_name"
 #set :repo_url, "git@example.com:me/my_repo.git"
 
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -23,7 +23,7 @@ set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 ## Defaults:
 # set :scm,           :git
-# set :branch,        :master
+set :branch,        :develop
 # set :format,        :pretty
 # set :log_level,     :debug
 # set :keep_releases, 5
@@ -40,36 +40,3 @@ namespace :puma do
   end
   before :start, :make_dirs
 end
-# namespace :deploy do
-#   # desc "Make sure local git is in sync with remote."
-#   # task :check_revision do
-#   #   on roles(:app) do
-#   #     unless `git rev-parse HEAD` == `git rev-parse origin/master`
-#   #       puts "WARNING: HEAD is not the same as origin/master"
-#   #       puts "Run `git push` to sync changes."
-#   #       exit
-#   #     end
-#   #   end
-#   # end
-#   desc 'Initial Deploy'
-#   task :initial do
-#     on roles(:app) do
-#       before 'deploy:restart', 'puma:start'
-#       invoke 'deploy'
-#     end
-#   end
-#   desc 'Restart application'
-#   task :restart do
-#     on roles(:app), in: :sequence, wait: 5 do
-#       invoke 'puma:restart'
-#     end
-#   end
-#   #before :starting,     :check_revision
-#   after  :finishing,    :compile_assets
-#   after  :finishing,    :cleanup
-#   after  :finishing,    :restart
-# end
-# ps aux | grep puma    # Get puma pid
-# kill -s SIGUSR2 pid   # Restart puma
-# kill -s SIGTERM pid   # Stop puma
-
