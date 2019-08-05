69f530fe745d8e101e865953d20655a6683d574f
Yegor Danchenko <yegordanchenko@gmail.com>
2019-08-05 22:03:42 +0300

    Work without confirmation

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,10 +4,11 @@ Devise.secret_key = 'a1cb0e9bee716d89935b31acdf6d2a36782f9596983ca1f1f990b9e323f
 
 Devise.setup do |config|
   # Required so users don't lose their carts when they need to confirm.
-  config.allow_unconfirmed_access_for = 1.days
+  config.allow_unconfirmed_access_for = 2.days
 
   # Fixes the bug where Confirmation errors result in a broken page.
   config.router_name = :spree
 
   # Add any other devise configurations here, as they will override the defaults provided by spree_auth_devise.
+  config.confirm_within = 2.days
 end
