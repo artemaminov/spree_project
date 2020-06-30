4840b7104e4ce9e6f7329094125f96673b330a83
artemaminov <artemaminov@gmail.com>
2020-06-30 16:33:33 +0500

    To use ES6 syntax, harmony mode enabled for staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -25,7 +25,7 @@ set :puma_worker_timeout, nil
 set :puma_init_active_record, true # Change to false when not using ActiveRecord
 ## Defaults:
 # set :scm,           :git
-set :branch, :master
+set :branch, :content_editor
 # set :format,        :pretty
 # set :log_level,     :debug
 # set :keep_releases, 5
