3308725b5b9e252183b7db4ecb685c6d01db97ae
artemaminov <artemaminov@gmail.com>
2020-08-19 11:33:13 +0500

    change out_of_stock to backorder

--- app/helpers/spree/frontend_helper_decorator.rb
+++ app/helpers/spree/frontend_helper_decorator.rb
@@ -15,6 +15,6 @@ Spree::FrontendHelper.module_eval do
   end
 
   def in_stock_text(product)
-    product.in_stock? ? Spree.t('in_stock') : Spree.t('out_of_stock')
+    product.in_stock? ? Spree.t('in_stock') : Spree.t('backorder')
   end
 end
