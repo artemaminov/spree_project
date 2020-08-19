373ff07a7bf20864cfcbcadcd160002394ca3647
artemaminov <artemaminov@gmail.com>
2020-08-19 12:10:48 +0500

    style breadcrumbs

--- app/helpers/spree/frontend_helper_decorator.rb
+++ app/helpers/spree/frontend_helper_decorator.rb
@@ -2,19 +2,22 @@ Spree::FrontendHelper.module_eval do
   def spree_breadcrumbs(taxon, separator = '/')
     return '' if current_page?('/') || taxon.nil?
 
-    separator = raw(separator)
-    crumbs = [content_tag(:span, link_to(content_tag(:span, Spree.t(:home), itemprop: 'name'), spree.root_path, itemprop: 'url') + separator, itemprop: 'item')]
+    separator = raw(content_tag(:span, separator))
+    crumbs = [link_to(Spree.t(:home), spree.root_path, itemprop: 'url') + separator]
     if taxon
-      crumbs << content_tag(:span, link_to(content_tag(:span, Spree.t(:products), itemprop: 'name'), spree.products_path, itemprop: 'url') + separator, itemprop: 'item')
-      crumbs << taxon.ancestors.collect { |ancestor| content_tag(:span, link_to(content_tag(:span, ancestor.name, itemprop: 'name'), seo_url(ancestor), itemprop: 'url') + separator, itemprop: 'item') } unless taxon.ancestors.empty?
-      crumbs << content_tag(:span, link_to(content_tag(:span, taxon.name, itemprop: 'name'), seo_url(taxon), itemprop: 'url'), itemprop: 'item')
+      # crumbs << link_to(Spree.t(:products), spree.products_path, itemprop: 'url') + separator
+      crumbs << taxon.ancestors.collect { |ancestor| link_to(ancestor.name, seo_url(ancestor), itemprop: 'url') + separator } unless taxon.ancestors.empty?
+      crumbs << link_to(taxon.name, seo_url(taxon), itemprop: 'url')
+      if @product
+        crumbs << separator + content_tag(:span, @product.name)
+      end
     else
-      crumbs << content_tag(:span, Spree.t(:products), itemprop: 'item')
+      crumbs << Spree.t(:products)
     end
     content_tag(:div, raw(crumbs.flatten.map(&:mb_chars).join), class: 'breadcrumbs', itemscope: 'itemscope', itemtype: 'https://schema.org/BreadcrumbList')
   end
 
   def in_stock_text(product)
-    product.in_stock? ? Spree.t('in_stock') : Spree.t('backorder')
+    product.in_stock? ? Spree.t('in_stock') : Spree.t('out_of_stock')
   end
 end
