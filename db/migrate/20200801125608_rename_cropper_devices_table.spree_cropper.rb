4f7cc6eeced67ae1d917da3cfba26b697f9c833f
artemaminov <artemaminov@gmail.com>
2020-08-25 13:41:38 +0500

    remove migration duplicate

--- db/migrate/20200801125608_rename_cropper_devices_table.spree_cropper.rb
+++
@@ -1,6 +0,0 @@
-# This migration comes from spree_cropper (originally 20200801104050)
-class RenameCropperDevicesTable < ActiveRecord::Migration[5.2]
-  def change
-    rename_table :spree_cropper_devices, :spree_cropper_dimensions
-  end
-end
