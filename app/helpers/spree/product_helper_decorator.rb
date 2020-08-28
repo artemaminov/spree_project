c999a12639399310a1745d36d5bf5fb85354a55d
artemaminov <artemaminov@gmail.com>
2020-08-28 09:57:42 +0500

    fix variant_icon

--- app/helpers/spree/product_helper_decorator.rb
+++ app/helpers/spree/product_helper_decorator.rb
@@ -10,7 +10,7 @@ Spree::ProductsHelper.module_eval do
   def variant_icon(variant)
     icon = variant.option_value('format', :icon)
     icons_path = ICONS_FOLDER + icon
-    if icon.empty? || Rails.application.assets_manifest.assets[icons_path].present?
+    if icon.empty? || Rails.application.assets_manifest.assets[icons_path].nil?
       image_tag ICONS_FOLDER + 'icon-add-product.svg'
     else
       image_tag icons_path
