5ceef6866338448a812fc227bcc2ba9f7e1bec2c
artemaminov <artemaminov@gmail.com>
2020-08-28 15:01:08 +0500

    cleanup, add comments

--- app/helpers/spree/product_helper_decorator.rb
+++ app/helpers/spree/product_helper_decorator.rb
@@ -2,6 +2,7 @@ Spree::ProductsHelper.module_eval do
   ICONS_FOLDER = 'icons/format/'
   FALLBACK_ICON = 'icon-add-product.svg'
 
+  # Get all option value in one line semicolon divided
   def option_values_text(product)
     options = fetch_options(product)
     options.join(', ')
@@ -18,6 +19,7 @@ Spree::ProductsHelper.module_eval do
     end
   end
 
+  # Compile product dimensions popover
   def formats_dimensions(product)
     options = Spree::OptionValue.where(presentation: fetch_options(product))
     output = %{
@@ -38,6 +40,7 @@ Spree::ProductsHelper.module_eval do
     output += '</div>'
   end
 
+  # Fetch all product variants format option values
   def fetch_options(product)
     options = []
     product.variants_and_option_values(current_currency).each do |variant|
