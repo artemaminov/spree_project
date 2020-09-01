416b76ba5690cbb8faed2d14322463eab089340f
artemaminov <artemaminov@gmail.com>
2020-09-01 17:34:12 +0500

    move helpers from product to frontend

--- app/helpers/spree/frontend_helper_decorator.rb
+++ app/helpers/spree/frontend_helper_decorator.rb
@@ -20,4 +20,40 @@ Spree::FrontendHelper.module_eval do
   def in_stock_text(product)
     product.in_stock? ? Spree.t('in_stock') : Spree.t('backorder')
   end
+
+  # Compile product dimensions popover
+  def formats_dimensions(product)
+    options = Spree::OptionValue.where(presentation: fetch_options(product))
+    output = %{
+      <div class='popover_products'>
+        <div class='item item_header'>
+          <div class='title'>Форматы</div>
+          <div class='format'>Размеры</div>
+        </div>
+    }
+    options.each do |option|
+      output += %{
+        <div class='item'>
+          <div class='title'>#{option.presentation}</div>
+          <div class='format'>#{option.dimension} мм</div>
+        </div>
+      }
+    end
+    output += '</div>'
+  end
+
+  # Fetch all product variants format option values
+  def fetch_options(product)
+    options = []
+    product.variants_and_option_values(current_currency).each do |variant|
+      options << variant.option_value('format')
+    end
+    options
+  end
+
+  # Get all option value in one line semicolon divided
+  def option_values_text(product)
+    options = fetch_options(product)
+    options.join(', ')
+  end
 end
--- app/helpers/spree/product_helper_decorator.rb
+++ app/helpers/spree/product_helper_decorator.rb
@@ -2,12 +2,6 @@ Spree::ProductsHelper.module_eval do
   ICONS_FOLDER = 'icons/format/'
   FALLBACK_ICON = 'icon-add-product.svg'
 
-  # Get all option value in one line semicolon divided
-  def option_values_text(product)
-    options = fetch_options(product)
-    options.join(', ')
-  end
-
   # Get variant icon if present or stub
   def variant_icon(variant)
     icon = variant.option_value('format', :icon) || ''
@@ -19,33 +13,4 @@ Spree::ProductsHelper.module_eval do
     end
   end
 
-  # Compile product dimensions popover
-  def formats_dimensions(product)
-    options = Spree::OptionValue.where(presentation: fetch_options(product))
-    output = %{
-      <div class='popover_products'>
-        <div class='item item_header'>
-          <div class='title'>Форматы</div>
-          <div class='format'>Размеры</div>
-        </div>
-    }
-    options.each do |option|
-      output += %{
-        <div class='item'>
-          <div class='title'>#{option.presentation}</div>
-          <div class='format'>#{option.dimension} мм</div>
-        </div>
-      }
-    end
-    output += '</div>'
-  end
-
-  # Fetch all product variants format option values
-  def fetch_options(product)
-    options = []
-    product.variants_and_option_values(current_currency).each do |variant|
-      options << variant.option_value('format')
-    end
-    options
-  end
 end
