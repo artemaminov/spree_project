875c3c80c97e9d7a46b86e2756f4dc906629f30c
artemaminov <artemaminov@gmail.com>
2020-09-02 13:47:53 +0500

    use taxon_to_return_to

--- app/models/spree/gallery.rb
+++ app/models/spree/gallery.rb
@@ -1,30 +1,34 @@
 module Spree
-    class Gallery < Spree::Base
-        validates :title, :subtitle, presence: true
+  class Gallery < Spree::Base
+    validates :title, :subtitle, presence: true
 
-        has_many :gallery_products, class_name: "Spree::GalleryProduct"
-        has_many :products, class_name: "Spree::Product",
-                 through: :gallery_products
+    has_many :gallery_products, class_name: "Spree::GalleryProduct"
+    has_many :products, class_name: "Spree::Product",
+             through: :gallery_products
 
-        has_one_attached :preview_image
-        has_one_attached :main_image
-        has_many_attached :images
+    has_one_attached :preview_image
+    has_one_attached :main_image
+    has_many_attached :images
 
-        if defined?(SpreeGlobalize)
-            translates :title, :subtitle, :desc, fallbacks_for_empty_translations: true
-            # include SpreeGlobalize::Translatable
-        end
-
-        def return_to_catalog
-            @return_to_catalog = products.first.taxons.first
-        end
-
-        def next
-            Spree::Gallery.where("id > ?", id).order("id ASC").first || Spree::Gallery.first
-        end
-
-        def previous
-            Spree::Gallery.where("id < ?", id).order("id DESC").first || Spree::Gallery.last
-        end
+    if defined?(SpreeGlobalize)
+      translates :title, :subtitle, :desc, fallbacks_for_empty_translations: true
+      # include SpreeGlobalize::Translatable
     end
+
+    def return_to_catalog
+      if products.any?
+        @return_to_catalog = products.first.taxon_to_return_to
+      else
+        Spree::Taxon.first
+      end
+    end
+
+    def next
+      Spree::Gallery.where("id > ?", id).order("id ASC").first || Spree::Gallery.first
+    end
+
+    def previous
+      Spree::Gallery.where("id < ?", id).order("id DESC").first || Spree::Gallery.last
+    end
+  end
 end
--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,13 +1,11 @@
 module Spree
-  Product.class_eval do
-    # acts_as_list :order => :position
-  end
-
   module ProductDecorator
-
-
     def lowest_price_variant
       prices.min_by { |v| v.amount }
+    end
+
+    def taxon_to_return_to
+      taxons.first
     end
   end
 end
