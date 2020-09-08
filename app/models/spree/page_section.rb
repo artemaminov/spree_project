dfb7c60955a8df3a1e00137ef12c6a22692a3274
artemaminov <artemaminov@gmail.com>
2020-09-08 16:35:14 +0500

    add ImageCombine to Slide and PageSection

--- app/models/spree/page_section.rb
+++ app/models/spree/page_section.rb
@@ -1,8 +1,11 @@
 module Spree
   class PageSection < Spree::Base
+    has_one :image, class_name: 'Spree::ImageCombine', as: :combinable
 
     validates :title, :description, :html_section_name, presence: true
     validates :button_url, presence: true, if: :button_text?
+
+    accepts_nested_attributes_for :image
 
     if defined?(SpreeGlobalize)
       translates :title, :description, :button_text, fallbacks_for_empty_translations: true
--- app/models/spree/slide.rb
+++ app/models/spree/slide.rb
@@ -1,8 +1,10 @@
 module Spree
   class Slide < Spree::Base
     has_and_belongs_to_many :sliders, :class_name => 'Spree::Slider', join_table: 'spree_sliders_slides'
+    has_one :image, class_name: 'Spree::ImageCombine', as: :combinable
 
     accepts_nested_attributes_for :sliders
+    accepts_nested_attributes_for :image
 
     validates_presence_of :title, :message, :order
 
