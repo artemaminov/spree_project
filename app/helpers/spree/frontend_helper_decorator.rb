90a80b04968aa8d87e01c393e9f6288d678c6005
artemaminov <artemaminov@gmail.com>
2020-09-10 15:12:51 +0500

    improve breadcrumbs

--- app/helpers/spree/frontend_helper_decorator.rb
+++ app/helpers/spree/frontend_helper_decorator.rb
@@ -1,28 +1,33 @@
 Spree::FrontendHelper.module_eval do
-  def spree_breadcrumbs(crumb, child = '')
+  def spree_breadcrumbs(crumb, child = '', options = { class: 'breadcrumbs' })
     return '' if current_page?('/') || crumb.nil?
 
     separator = '/'
 
     separator = raw(content_tag(:span, separator))
     crumbs = [link_to(Spree.t(:home), spree.root_path, itemprop: 'url') + separator]
+
     if crumb.is_a?(Spree::Taxon)
       # crumbs << link_to(Spree.t(:products), spree.products_path, itemprop: 'url') + separator
       crumbs << crumb.ancestors.collect { |ancestor| link_to(ancestor.name, seo_url(ancestor), itemprop: 'url') + separator } unless crumb.ancestors.empty?
-      crumbs << link_to(crumb.name, seo_url(crumb), itemprop: 'url')
+
       if @product
+        crumbs << link_to(crumb.name, seo_url(crumb), itemprop: 'url')
         crumbs << separator + content_tag(:span, @product.name)
+      else
+        crumbs << content_tag(:span, crumb.name)
       end
     else
+
       if child.blank?
         crumbs << content_tag(:span, Spree.t(crumb))
       else
-        crumbs << link_to(Spree.t(crumb), '', itemprop: 'url') + separator
+        crumbs << link_to(Spree.t(crumb), "/#{controller_name}", itemprop: 'url') + separator
         crumbs << content_tag(:span, child)
       end
       # crumbs << content_tag(:span, Spree.t(:products))
     end
-    content_tag(:div, raw(crumbs.flatten.map(&:mb_chars).join), class: 'breadcrumbs', itemscope: 'itemscope', itemtype: 'https://schema.org/BreadcrumbList')
+    content_tag(:div, raw(crumbs.flatten.map(&:mb_chars).join), options, itemscope: 'itemscope', itemtype: 'https://schema.org/BreadcrumbList')
   end
 
   def in_stock_text(product)
