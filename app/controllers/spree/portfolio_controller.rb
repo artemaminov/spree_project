953810b92fb5d8dc2a4c361b80b9ac07a49b890e
Vladimir Petrachuk <hippozz@yandex.ru>
2020-09-11 17:15:36 +0500

    pagination portfolio

--- app/controllers/spree/portfolio_controller.rb
+++ app/controllers/spree/portfolio_controller.rb
@@ -1,14 +1,21 @@
 class Spree::PortfolioController < Spree::StoreController
   def index
-    @portfolio = Spree::Gallery.all
-                         .order(position: :asc)
-                         .page(params[:page])
+    @portfolio = Spree::Gallery
+                     .all
+                     .order(position: :asc)
+                     .page(params[:page])
+                     .per(10)
+
     @use_light_nav_bar = true
   end
 
   def show
     @portfolio = Spree::Gallery.find params[:id]
-    @next_portfolio = @portfolio.next
+    @next_portfolio = Spree::Gallery
+                          .where('position >= ?', @portfolio.position)
+                          .where.not(id: @portfolio.id)
+                          .order(position: :asc)
+                          .first
   end
 
 end
