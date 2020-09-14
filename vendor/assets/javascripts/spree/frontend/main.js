a50294bd92cbe24c412a2d6205157dba8ff69b7f
artemaminov <artemaminov@gmail.com>
2020-09-14 11:12:22 +0500

    fix calc

--- vendor/assets/javascripts/spree/frontend/main.js
+++ vendor/assets/javascripts/spree/frontend/main.js
@@ -38,7 +38,7 @@ function calc(e) {
       variantSqrMeters,
       variantPallets,
       variantTotal = 0;
-  var variant = $(e).data("params");
+  var variant = $(e).data("info");
   var amount = $(e).val();
   var variantPiecesElement = $("#variant-".concat(variant.id, "-pieces"));
   var variantPalletsElement = $("#variant-".concat(variant.id, "-pallets"));
